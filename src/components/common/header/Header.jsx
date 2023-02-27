75708057e9b121cda3b6ca744d7f4d43f49d9e36
PaulaDuduta <100792374+PaulaDuduta@users.noreply.github.com>
2023-02-27 18:54:49 +0200

    fixes

--- src/components/common/header/Header.jsx
+++ src/components/common/header/Header.jsx
@@ -1,22 +1,20 @@
-import React from 'react';
-import { GiAbstract037 } from 'react-icons/gi';
+import { FiMeh } from 'react-icons/fi';
 import { Link } from 'react-router-dom';
+import { UserControls } from '.';
 
 export const Header = () => {
   return (
     <div className="container mx-auto px-4 flex justify-between">
       <h1 className="uppercase font-bold text-lg inline-block">
-        <Link
-          to="/"
-          title="Home"
-          className="text-teal-900 flex gap-3 items-center"
-        >
-          <GiAbstract037 size={32} className="inline-block"></GiAbstract037>
+        <Link to="/" title="Home" className="flex gap-3 items-center">
+          <FiMeh size={24} className="inline-block"></FiMeh>
           Game Shell
         </Link>
       </h1>
 
-      <div>controls</div>
+      <div>
+        <UserControls></UserControls>
+      </div>
     </div>
   );
 };
--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,7 +1,10 @@
+import { useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { FullScreenLayout } from '../layouts';
 
 export const FourOhFour = () => {
+  const dispatch = useDispatch();
+
   return (
     <FullScreenLayout>
       <div className="bg-teal-500 w-screen h-screen flex items-center justify-center">
@@ -12,6 +15,17 @@ export const FourOhFour = () => {
             Home
           </Link>
         </div>
+
+        <button
+          onClick={() => {
+            dispatch({
+              type: 'add',
+              payload: 404,
+            });
+          }}
+        >
+          Add 404 to state
+        </button>
       </div>
     </FullScreenLayout>
   );
